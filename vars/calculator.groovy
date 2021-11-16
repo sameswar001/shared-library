ea8c983f4e9fda5cac1c87083e49e88a9bba68d7
sam123 <sameswar001@gmail.com>
2021-11-16 22:57:28 +0100

    added two method

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,3 +1,10 @@
 def add(x,y){
   echo "Sum of ${x} and ${y} is ${x+y}" 
 }
+def multiply(x,y){
+  echo "Sum of ${x} and ${y} is ${x*y}"
+}
+
+def division(x,y){
+  echo "Sum of ${x} and ${y} is ${x/y}"
+}
